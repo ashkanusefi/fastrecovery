ab74871b8c27f8e86df6fdd2e6a3e0d27a6e213b
ashkanusefi <yousefi.ashkan96@gmail.com>
2021-06-11 11:13:25 +0330

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path("faq/", views.faq, name="faq"),
+    path("about/",views.about,name="about"),
+    path("contact",views.contact,name="contact"),
+]
---
+++ home/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def index(request):
+    s = False
+    if request.method == 'POST':
+        if "contact-name" in request.POST.keys():
+            messages = request.POST['contact-name'] + "\n" + \
+                       request.POST['contact-email'] + "\n" + \
+                       request.POST['contact-phone'] + "\n" + \
+                       request.POST['contact-message']
+
+            send_mail("CONTACT FORM",
+                      messages,
+                      settings.EMAIL_HOST_USER,
+                      ['support@fast-recovery.org'],
+                      fail_silently=False
+                      )
+            s = True
+        elif "send-message" in request.POST.keys():
+            messages = request.POST['send-name'] + "\n" + \
+                       request.POST['send-email'] + "\n" + \
+                       request.POST['send-message']
+
+            send_mail("SEND MESSAGE FORM",
+                      messages,
+                      settings.EMAIL_HOST_USER,
+                      ['support@fast-recovery.org'],
+                      fail_silently=False
+                      )
+            s = True
+
+        else:
+            messsages = request.POST["ex-name"] + "\n" + \
+                        request.POST["ex-company"] + "\n" + \
+                        request.POST["ex-phone"] + "\n" + \
+                        request.POST["ex-email"] + "\n" + \
+                        request.POST["ex-currency"] + "\n" + \
+                        request.POST["ex-amount"] + "\n" + \
+                        request.POST["ex-country"] + "\n" + \
+                        request.POST["ex-message"]
+
+            send_mail("EXAMINATION FORM",
+                      messsages,
+                      settings.EMAIL_HOST_USER,
+                      ["support@fast-recovery.org"],
+                      fail_silently=False
+                      )
+            s = True
+
+    return render(request, 'home/base.html', {"s": s})
+
+
+def faq(request):
+    context = {
+    }
+    return render(request, "home/faq.html", context)
+
+
+def about(request):
+    context = {
+    }
+    return render(request, "home/about.html", context)
+
+
+def contact(request):
+    context = {
+    }
+    return render(request, "home/contact.html", context)
